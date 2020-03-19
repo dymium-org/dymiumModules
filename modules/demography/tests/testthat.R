fdcf26f1de15c100325e41b6c44ec6c1043b3ec0
amarin <17020181+asiripanich@users.noreply.github.com>
2020-03-19 19:36:11 +1100

    test: remove all test scripts from module folder

--- modules/demography/tests/testthat.R
+++
@@ -1,8 +0,0 @@
-library(testthat)
-test_check("demography")
-
-# to test all the test cases run below
-# test_dir("modules/demography/tests/testthat")
-
-# or test a specific test file
-# test_file("modules/demography/tests/testthat/test.R")
--- modules/demography/tests/testthat/setup.R
+++
@@ -1,3 +0,0 @@
-library(dymiumCore)
-dymiumCore:::lg$set_threshold(level = "warn")
-helpers <- modules::use(here::here('modules/demography/helpers.R'))
